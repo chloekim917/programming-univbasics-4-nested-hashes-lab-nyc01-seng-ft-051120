0dae2b352766ba76c5d091231160fc680d3130c5
Seoyoung Kim <mintholic1@gmail.com>
2020-04-23 20:23:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,6 +94,7 @@ def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
 
+  alans_new_info
 	programmer_hash = 
  		{
       :grace_hopper => {
